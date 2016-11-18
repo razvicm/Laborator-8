c04a1c9c0a5b6429f2ee63b345d03b13903209cd
Razvan <rciupei@yahoo.com>
2016-11-18 18:22:09 +0200

    Exercitiul 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include  <ctime>
+#include  <fstream>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ void ordonCresc(int [], int);
 void ordonDesc(int [], int);
 void invers(int [], int);
 void inter(int [], int [], int [], int, int);
+int prezent(int, int [], int);
+void divizibil(int [], int, int &, int &, int &);
+void scriu(int [], int);
 
 int main()
 {
@@ -52,6 +56,20 @@ int main()
 	cout<<"Sirul c este: ";
 	for(int i = 0; i < 2*n; i++)
 	   cout << c[i] << ", ";
+    int nrdiv3, nrdiv5, nrdiv3si5;
+	divizibil(c, 2*n, nrdiv3, nrdiv5, nrdiv3si5);
+	cout<<endl;
+	cout << "Divizibile prin 3: " << nrdiv3 << " numere," << endl
+	     << "Divizibile prin 5: " << nrdiv5 << " numere," << endl
+	     << "Divizibile prin 3 si 5: " << nrdiv3si5 << " numere." << endl;
+    cout<<endl;
+    int v = 12;
+	if (prezent(v, c, 2*n) > 0)
+		cout << v << " este prezent in sirul c." << endl;
+	else
+		cout << v << " nu este prezent in sirul c." << endl;
+    scriu(c, 2*n);
+
 	return 0;
 }
 
@@ -134,4 +152,43 @@ void inter(int a[], int b[], int rez[], int na, int nb)
 			}
 	}
 	while(i + j < na + nb);
+}
+
+void divizibil(int a[], int n, int& nrdiv3, int& nrdiv5, int& nrdiv3si5){
+    int i;
+    nrdiv3=nrdiv5=nrdiv3si5=0;
+        for(i=0; i<n;i++){
+            if (a[i]%3==0)
+            nrdiv3++;
+            if (a[i]%5==0)
+            nrdiv5++;
+            if (a[i]%3+a[i]%5==0)
+            nrdiv3si5++;
+        }
+}
+int prezent(int x, int v[], int n)
+{
+   int mini, maxi, mijloc;
+   mini = 0;
+   maxi = n - 1;
+   while (mini <= maxi)
+   {
+      mijloc = (mini + maxi) / 2;
+      if (x < v[mijloc])
+         maxi = mijloc - 1;
+      else if (x > v[mijloc])
+         mini = mijloc + 1;
+      else
+         return mijloc;    //  Gasit!
+   }
+   return -1;              //  Absent!
+}
+void scriu(int a[],int n){
+    fstream fd;
+   int i;
+   fd.open( "fdate.txt", ios::out);
+    for(i=0;i<n-1;i++)
+        fd<<a[i]<<" ,";
+    fd<< a[n-1];
+   fd.close();
 }
